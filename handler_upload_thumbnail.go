d7dc213d49539f7eaf3586a5cef5a204387e3475
RussNavas <navasr@oregonstate.edu>
2026-02-07 17:26:28 -0800

    implemented thumbnails

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	//"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -31,7 +33,49 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20;
+	r.ParseMultipartForm(maxMemory);
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail");
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err);
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type");
+	imgs, err := io.ReadAll(file);
+	if err != nil{
+		respondWithError(w, http.StatusInternalServerError, "Unable to read image daata", err)
+		return
+	}
+
+	vid, err := cfg.db.GetVideo(videoID);
+	if err != nil{
+		respondWithError(w, http.StatusNotFound, "Video not found in db", err)
+		return
+	}
+
+	if vid.UserID != userID{
+		respondWithError(w, http.StatusUnauthorized, "User is not the video owner", err)
+		return
+	}
+
+	newThumbnail := thumbnail{
+		data: imgs,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = newThumbnail;
+
+	formattedURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID);
+	vid.ThumbnailURL = &formattedURL
+	err = cfg.db.UpdateVideo(vid);
+	if err != nil{
+		delete(videoThumbnails, videoID)
+		respondWithError(w, http.StatusInternalServerError, "unable to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, vid);
 }
